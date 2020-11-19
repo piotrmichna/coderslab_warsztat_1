6031d7b09961eaeae407890850816d3a8e87609c
Piotr Michna <pm@piotrmichna.pl>
2020-11-19 21:19:18 +0100

    defined short main fun guess_the_number

--- guess_number/gues_number.py
+++ guess_number/gues_number.py
@@ -1,2 +1,13 @@
 def get_user_number():
     return int(input('Guess the number: '))
+
+def guess_the_number():
+    secret_number=randint(1,100)
+
+    while True:
+        usr_number=get_user_number()
+        if secret_number==usr_number:
+            print('Great, you are a winner')
+            break
+        else
+            print('You missed the number..')
