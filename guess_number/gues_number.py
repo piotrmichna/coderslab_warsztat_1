c7e8004cfe770710733755e068eaad6d2e81f1f5
Piotr Michna <pm@piotrmichna.pl>
2020-11-19 21:25:12 +0100

    testing and formating code

--- guess_number/gues_number.py
+++ guess_number/gues_number.py
@@ -1,15 +1,21 @@
 from random import randint
 
+
 def get_user_number():
     return int(input('Guess the number: '))
 
+
 def guess_the_number():
-    secret_number=randint(1,100)
+    secret_number = randint(1, 100)
 
     while True:
-        usr_number=get_user_number()
-        if secret_number==usr_number:
+        usr_number = get_user_number()
+        if secret_number == usr_number:
             print('Great, you are a winner')
             break
-        else
+        else:
             print('You missed the number..')
+
+
+if __name__ == '__main__':
+    guess_the_number()
